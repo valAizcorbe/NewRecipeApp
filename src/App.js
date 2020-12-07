c0d14463bb12ed18043e39caafd08ab6aba66103
valAizcorbe <valeria.aizcorbe@hotmail.com>
2020-12-07 12:15:40 -0700

    get request

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react"
 import './App.css';
+import Recipe from './Recipe'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+  const APP_ID = '407bd9a8';
+  const APP_KEY = '7680065f6d54f399eb2563c5a183f072'
+
+  const [recipes, setRecipes] = useState([])
+  const [search, setSearch] = useState('')
+  const [query, setQuery] = useState ('chicken')
+
+useEffect(() => {
+  getRecipes();
+}, [query]);
+
+const getRecipes = async () => {
+const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
+const data = await response.json();
+setRecipes(data.hits);
+}
+
+const getSearch = event => {
+  event.preventDefault(); 
+  setQuery(search);
+  setSearch('')
+}
+
+return (
+  <div className='App'>
+<form  className="search-form" onSubmit={getSearch} >
+  <input className='input-text' type ='text' value={search} onChange={(e) => setSearch(e.target.value)}/>
+  <button type='submit' className='button-search' >Search</button>
+  </form>  
+ {recipes.map(recipe => (
+   <Recipe 
+   title={recipe.recipe.label} 
+   calories={recipe.recipe.calories}
+   image={recipe.recipe.image}/>
+ ))}
+  </div>
+)
 }
 
 export default App;
---
+++ src/Recipe.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Recipe = ({title, calories, image}) => {
+    return (
+        <div>
+            <h1>Title: {title}</h1>
+    <p>Calories: {calories}</p>
+            <img src={image}/>
+        </div>
+    )
+}
+
+export default Recipe
